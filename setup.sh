5ac2c1e19a6499d76cd61d3b2f73d6af0afd689e
Nisreen Abu Zidan <nisreen@Ghassans-MBP.fritz.box>
2022-12-15 19:55:33 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-CLIENT_ID=n64t2zjtu69beyj8eth6fkr2 CLIENT_SECRET=CutaFC2cgvgHV4xXBmYh GRANT_TYPE=client_credentials docker-compose up
+
+
+docker-compose up -d mongodb
+
+MYSQL_ROOT_PASSWORD=root docker-compose up -d mySqldb
+
+CLIENT_ID=n64t2zjtu69beyj8eth6fkr2 CLIENT_SECRET=CutaFC2cgvgHV4xXBmYh GRANT_TYPE=client_credentials docker-compose up main
+
+docker exec -it main bash
+
+
+
+
+docker image build ./docker -t main:latest
+
+
+docker container run -it --name main --mount type=volume,src=app,dst=./code 
+docker container run -it main:latest
